da07eb12346954ab61b389279738d8aa19e6b1d2
Valenreynoso17 <valenreynoso17@gmail.com>
2021-05-11 09:56:46 -0300

    resuelvo paso 02

--- src/died/guia06/Alumno.java
+++ src/died/guia06/Alumno.java
@@ -3,7 +3,7 @@ package died.guia06;
 import java.util.List;
 
 
-public class Alumno {
+public class Alumno implements Comparable<Alumno>{
 
 	private String nombre;
 	private Integer nroLibreta;
@@ -21,6 +21,33 @@ public class Alumno {
 	public void inscripcionAceptada(Curso c) {
 		//
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) 	return true;
+		
+		if (!(obj instanceof Alumno))	return false;
+		
+		Alumno other = (Alumno) obj;
+		if (nroLibreta == null) {
+			if (other.nroLibreta != null)
+				return false;
+		} else if (!nroLibreta.equals(other.nroLibreta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Alumno o) {
+		return this.nombre.compareTo(o.getNombre());
+	}
+	
+	private String getNombre() {
+		return this.nombre;
+	}
+	
+	
+	
 	
 	
 
